f3425e27084feaffacb2867375abcbcd36634947
werdffelynir <werdffelynir@gmail.com>
2016-04-27 11:00:00 +0300

    init

--- templates/part.content.php
+++ templates/part.content.php
@@ -6,4 +6,86 @@
 
 ?>
 
-<h1>content</h1>
+
+<div class="tbl">
+<!--    left side    -->
+    <div class="tbl_cell">
+        <h1>Name of the Project (from Gantt-Chart)</h1>
+        <div class="tbl">
+            <div class="tbl_cell clb_fields">
+                <div class="tbl">
+                    <div class="tbl_cell">Client</div>
+                    <div class="tbl_cell"><input type="text" placeholder="Client name" name="project_client_name"></div>
+                </div>
+                <div class="tbl">
+                    <div class="tbl_cell">Venue</div>
+                    <div class="tbl_cell"><input type="text" placeholder="Description 1" name="project_description1"></div>
+                </div>
+                <div class="tbl">
+                    <div class="tbl_cell">&nbsp;</div>
+                    <div class="tbl_cell"><input type="text" placeholder="Description 2" name="project_description2"></div>
+                </div>
+                <div class="tbl">
+                    <div class="tbl_cell">&nbsp;</div>
+                    <div class="tbl_cell"><input type="text" placeholder="Street + Number" name="project_sreet-"></div>
+                </div>
+                <div class="tbl">
+                    <div class="tbl_cell">&nbsp;</div>
+                    <div class="tbl_cell"><input type="text" placeholder="ZIP" name="project_zip"> <input type="text" placeholder="City" name="project_city"></div>
+                </div>
+                <div class="tbl">
+                    <div class="tbl_cell">&nbsp;</div>
+                    <div class="tbl_cell"><input type="text" placeholder="Country as Dropdown=list"></div>
+                </div>
+            </div>
+
+
+            <div class="tbl_cell">
+                <div class="clb_logo">clb_logo</div>
+                <div class="tbl">
+                    <div class="tbl_cell">Upload</div>
+                    <div class="tbl_cell">Choose</div>
+                </div>
+            </div>
+        </div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+
+
+<!--    right side    -->
+    <div class="tbl_cell">
+        <h1>Statistic</h1>
+        <div>
+            <div class="tbl">
+                <div class="tbl_cell">100</div>
+                <div class="tbl_cell">of</div>
+                <div class="tbl_cell">50</div>
+                <div class="tbl_cell">tasks finished ... <a href="#">finish a task</a></div>
+            </div>
+            <div class="tbl">
+                <div class="tbl_cell">100</div>
+                <div class="tbl_cell">of</div>
+                <div class="tbl_cell">50</div>
+                <div class="tbl_cell">tasks finished ... <a href="#">finish a task</a></div>
+            </div>
+            <div class="tbl">
+                <div class="tbl_cell">100</div>
+                <div class="tbl_cell">of</div>
+                <div class="tbl_cell">50</div>
+                <div class="tbl_cell">tasks finished ... <a href="#">finish a task</a></div>
+            </div>
+            <div class="tbl">
+                <div class="tbl_cell">100</div>
+                <div class="tbl_cell">of</div>
+                <div class="tbl_cell">50</div>
+                <div class="tbl_cell">tasks finished ... <a href="#">finish a task</a></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
